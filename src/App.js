533edcf2d800cd3868449b2958760d6c5f3dd2c7
LaiFrance <laise94@hotmail.com>
2023-12-04 20:10:55 -0300

    add style

--- src/App.js
+++ src/App.js
@@ -22,7 +22,7 @@ function App() {
     <Router>
       <Routes>
         <Route path="/login" element={<LoginPage />} />
-        <Route path="/signup" element={<RegisterPage />} />
+        <Route path="/register" element={<RegisterPage />} />
         <Route path="/" element={<RegisterPage />} />
       </Routes>
     </Router>
